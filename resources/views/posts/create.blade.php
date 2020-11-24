03aa1b3007d2f4be6cac746d0067920ad656a624
hironobu0824 <banu.asika0824.nobu@gmail.com>
2020-11-24 16:43:29 +0900

    デザイン

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,32 +7,40 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog name</h1>
+        <p class="blogname">高橋宏暢のブログ</p>
         <form action="/posts" method="POST">
             {{ csrf_field() }}
             <div class="title">
-                <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
-                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+                <p class="heading">Title</p>
+                <div class="content">
+                    <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                    <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+                </div>
             </div>
             <div class="body">
-                <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れ様でした。">{{ old('post.body') }}</textarea>
-                <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
+                <p class="heading">Body</p>
+                <div class="content"> 
+                    <textarea name="post[body]" placeholder="今日も1日お疲れ様でした。">{{ old('post.body') }}</textarea>
+                    <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
+                </div>
             </div>
-            <div class='content_category'>
-                <h2>カテゴリ</h2>
-                <select name="post[categories][]" multiple>
-                    @foreach ($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                    @endforeach
-                </select>
+            <div class='category'>
+                <p class="heading">カテゴリ</p>
+                <div class="content content_category">
+                    <select name="post[categories][]" multiple>
+                        <option value="" hidden>カテゴリを選択してください</option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
             </div>
             <input type="submit" value="store"/>
         </form>
-        <div class="back">[<a href="/">back</a>]</div>
+        <div class="back"><a href='/'><i class="fas fa-long-arrow-alt-left"></i></a></div>
     </body>
 </html>
